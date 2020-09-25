d7e2f2e962b7321cebd41243c2465015bd510270
ivo0nikolaev <ivo759678@gmail.com>
2020-09-25 02:13:24 +0200

    Added routers for User and Photo - jsut structure

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const port = process.env.PORT || 3000
+require("./db/mangoose");
 
-app.use(express.json())
+const Photo = require("./models/photo");
+const userRouter = require('./routers/user')
+const photoRouter = require('./routers/photo')
 
-app.get('/test', (req, res)=>{
-    console.log(req.body)
-    res.send('IT WORKS!', req.body)
-})
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.post("/users", (req, res) => {
-    const user = new User(req.body);
-    console.log("Req: ", req.body);
-    console.log(user);
-    user
-      .save()
-      .then(() => {
-        res.status(201).send(user);
-      })
-      .catch((error) => {
-        res.status(400).send(error);
-      });
-  });
-  
-app.listen(port, () =>{
-    console.log('Testing ... testing ...')
-})
+const router = new express.Router()
+router.get('/test')
+
+//It auto-parses json requests.
+
+app.use(express.json());
+app.use(userRouter)
+app.use(photoRouter)
+
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
---
+++ src/routers/photo.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const Photo = require("../models/photo");
+const router = new express.Router()
+
+module.exports = router

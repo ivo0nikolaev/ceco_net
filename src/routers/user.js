8eca95548972c3917c8d031824383607da7af215
ivo0nikolaev <ivo759678@gmail.com>
2020-10-06 01:06:33 +0200

    Added PATCH /users/me -update an user

--- src/routers/user.js
+++ src/routers/user.js
@@ -16,11 +16,36 @@ router.post("/users", async (req, res) => {
   }
 });
 
-
 //Get User
 router.get("/users/me", auth, async (req, res) => {
     res.send(req.user);
   });
+
+//Update The user
+router.patch("/users/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowed = ["name", "email", "password", "age", "status", "phone"]
+  
+  const isAllowed = updates.every((update) => allowed.includes(update))
+  
+  if(!isAllowed){
+    return res.status(400).send({error: "Invalid operation"})
+  }
+
+  try{
+    const user = await User.findById(req.user._id)
+    updates.forEach((update) => (user[update] = req.body[update]))
+    await user.save()
+
+    if (!user) {
+      return res.status(404).send()
+    }
+
+    res.send(user)
+  }catch(e){
+    res.status(500).send(e)
+  }
+})
 
 //Login a User
 router.post("/users/login", async (req, res) => {

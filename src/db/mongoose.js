cf61d19ba70214a30a6105764d8b31b7888d6ea7
Sclipper <mkopoi3@gmail.com>
2020-12-17 18:36:54 +0100

    new database

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/ceco-api", {
+mongoose.connect("mongodb+srv://root:root@cluster0.z8rrz.mongodb.net/Cluster0?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
-const express = require("express");
+const express = require("express")
 
-const mongoose = require("mongoose");
-
+const mongoose = require("mongoose")
+const cors = require("cors")
 mongoose.connect("mongodb://localhost:27017/ceco-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
-  useFindAndModify: true
-});
+  useFindAndModify: true,
+})
 
-const Photo = require("./models/photo");
-const userRouter = require('./routers/user')
-const photoRouter = require('./routers/photo')
+const Photo = require("./models/photo")
+const userRouter = require("./routers/user")
+const photoRouter = require("./routers/photo")
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3001
 
 const router = new express.Router()
-
-app.get('/test', (req, res) =>{
-  res.send('Cec-Net will send Mr. Roboto to kill Sarah Conor')
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+)
+app.get("/test", (req, res) => {
+  res.send("Cec-Net will send Mr. Roboto to kill Sarah Conor")
 })
 
 //It auto-parses json requests.
 
-app.use(express.json());
+app.use(express.json())
 app.use(userRouter)
 app.use(photoRouter)
 
 app.listen(port, () => {
-  console.log("Cec Net has become self aware on port: " + port);
-});
+  console.log("Cec Net has become self aware on port: " + port)
+})
